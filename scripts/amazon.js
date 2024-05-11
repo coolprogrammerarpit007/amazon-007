c299633ee619fc5fa4d49bf3545460ac9e45ee56
Arpit <arpit.mishra.out@gmail.com>
2024-05-11 07:35:40 +0530

    Added interactivity to addCart button and total quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -55,8 +55,46 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">Add to Cart</button>
+          <button class="add-to-cart-button button-primary add-product-cart" data-product-id="${
+            product.id
+          }">Add to Cart</button >
         </div>`;
 });
 
 productGrid.innerHTML = productsHTML;
+
+// Event Listener to the addCart Button
+const addCart = document.querySelectorAll(`.add-product-cart`);
+const cartQty = document.querySelector(`.cart-quantity`);
+cartQty.textContent = 0;
+
+addCart.forEach((cartBtn) => {
+  cartBtn.addEventListener(`click`, (e) => {
+    const productId = cartBtn.dataset.productId;
+
+    // check if product already exists in cart
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
+
+    // find the total products at cart
+    let total = 0;
+    cart.forEach((item) => {
+      total += item.quantity;
+    });
+    cartQty.textContent = total;
+  });
+});
