11c7886f76324047a736b1f921035cf2e87b916a
Arpit <arpit.mishra.out@gmail.com>
2024-05-22 07:52:04 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,28 @@
 `use strict`;
 import { products } from "./products.js";
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    qty: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    qty: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem(`cart`));
 
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      qty: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      qty: 1,
+    },
+  ];
+}
+
+// **************************************
+// **************************************
+
+// function for saving product to local storage.
+
+function saveToStorage() {
+  localStorage.setItem(`cart`, JSON.stringify(cart));
+}
 // ************************************
 // ************************************
 // Function for adding produts to the cart
@@ -37,6 +49,7 @@ export const addCartProducts = function (productId, i, itemQty) {
       qty: Number(itemQty),
     });
   }
+  saveToStorage();
 };
 
 // ************************************
@@ -52,4 +65,5 @@ export const removeFromCart = function (productId) {
     }
     cart = newCart;
   });
+  saveToStorage();
 };
